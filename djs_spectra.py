Added mask to fix Mars spectrum normalization issue

djs_spectra.py
@@ -5,8 +5,6 @@ from scipy.interpolate import interp1d
 import matplotlib
 import matplotlib.pyplot as plt
 
-
-
 #calculate JSD for two spectra
 def djs(spec1, spec2):
 
@@ -14,16 +12,17 @@ def djs(spec1, spec2):
 	pow1 = fft(spec1)
 	pow2 = fft(spec2)
  
-    #get modal fractions
+	#get modal fractions
 	p, q = pow1*np.conj(pow1)/sum(pow1*np.conj(pow1)), pow2*np.conj(pow2)/sum(pow2*np.conj(pow2))
 	p,q = np.real(p), np.real(q) #force Python to drop the "+0j" part of the "complex" number
-    
+	
 	#get D_JS of spectra
 	r = 1/2 * (p+q)
-	Djs = 1/2 * sum(p*np.log(p/r)) + 1/2 * sum(q*np.log(q/r))
+	p,r,q = np.ma.array(p), np.ma.array(r), np.ma.array(q)
+	Djs = 1/2 * np.sum(p*np.log(p/r)) + 1/2 * np.sum(q*np.log(q/r))
  
 	return Djs
- 
+	
  
 #calculate JSD density for two spectra
 def djs_density(spec1, spec2):
@@ -41,48 +40,48 @@ def djs_density(spec1, spec2):
    return djs_density
  
  
-    
-    
+	
+	
 #remove unphysical absorption and emission lines
 def clean(wave, flux):
-    new_wave, new_flux = [], []
-    for i in range(len(wave)-1):
-        if abs(flux[i] - flux[i+1]) < 0.5e-10:
-            new_flux.append(flux[i])
-            new_wave.append(wave[i])
-        
-    new_wave, new_flux = np.array(new_wave), np.array(new_flux)
+	new_wave, new_flux = [], []
+	for i in range(len(wave)-1):
+		if abs(flux[i] - flux[i+1]) < 0.5e-10:
+			new_flux.append(flux[i])
+			new_wave.append(wave[i])
+		
+	new_wave, new_flux = np.array(new_wave), np.array(new_flux)
 
-    return new_wave, new_flux
+	return new_wave, new_flux
 
 
 #get a
 def find_nearest(arr, value):
 
-    idx =  (np.abs(arr-value)).argmin()
+	idx =  (np.abs(arr-value)).argmin()
 
-    return arr[idx]
+	return arr[idx]
 
 
 #adapted from https://www.codegrepper.com/code-examples/python/python+find+nearest+point+in+array
 #get Djs density to match a wavelength value
 def find_nearest_Djs(wave_arr, djs_arr, wave_value):
 
-    idx = (np.abs(wave_arr-wave_value)).argmin()
+	idx = (np.abs(wave_arr-wave_value)).argmin()
 
-    return wave_arr[idx], djs_arr[idx]
+	return wave_arr[idx], djs_arr[idx]
 
 
 #truncates wavelength and flux arrays from the beginning and ending wavelengths in a wavelength packet
 def truncate(beg_wave, end_wave, wave_arr, flux_arr):
 
-    beg_idx, end_idx = (np.abs(wave_arr-beg_wave)).argmin(), (np.abs(wave_arr-end_wave)).argmin()
-    wave_arr, flux_arr = wave_arr[beg_idx:end_idx], flux_arr[beg_idx:end_idx]
+	beg_idx, end_idx = (np.abs(wave_arr-beg_wave)).argmin(), (np.abs(wave_arr-end_wave)).argmin()
+	wave_arr, flux_arr = wave_arr[beg_idx:end_idx], flux_arr[beg_idx:end_idx]
 
-    return wave_arr, flux_arr
+	return wave_arr, flux_arr
 
 
-    
+	
 #-------------------------------------------------------------
 #
 #                IMPORT SPECTRAL DATA
@@ -97,41 +96,82 @@ def truncate(beg_wave, end_wave, wave_arr, flux_arr):
 #earth_wave, earth_flux = np.loadtxt('CatalogofSolarSystemObjects/Spectra/R8resolution/Sun/Earth_Lundock081121_Spec_Sun_LoRes.txt', unpack=True)
 #mars_wave, mars_flux = np.loadtxt('CatalogofSolarSystemObjects/Spectra/R8resolution/Sun/Mars_McCord1971_Spec_Sun_LoRes.txt', unpack=True)
 #jupiter_wave, jupiter_flux = np.loadtxt('CatalogofSolarSystemObjects/Spectra/R8resolution/Sun/Jupiter_Lundock080507_Spec_Sun_LoRes.txt', unpack=True)
+
+
+def get_binned_interp(earth_wave, earth_flux, mars_wave, mars_flux, jupiter_wave, jupiter_flux):
+
+	#clean spectra
+	earth_wave, earth_flux = clean(earth_wave, earth_flux)
+	mars_wave, mars_flux = clean(mars_wave, mars_flux)
+	jupiter_wave, jupiter_flux = clean(jupiter_wave, jupiter_flux)
+
+
+	#bins = {'H2O':[0.52, 0.72], 'CO2': [1.42, 1.58], 'O3': [0.94, 0.99], 'CH4':[0.73, 0.81], 'O2':[0.59, 0.70]}
+	bins = {'H2O':[0.5, 0.7], 'CO2': [1.4, 1.6], 'O3': [0.9, 1.0], 'CH4':[0.7, 0.8], 'O2':[0.6, 0.70]}
+
+	interp_bins_earth =  {'H2O':[], 'CO2': [], 'O3': [], 'CH4':[], 'O2':[]}
+	interp_bins_mars =  {'H2O':[], 'CO2': [], 'O3': [], 'CH4':[], 'O2':[]}
+	interp_bins_jupiter =  {'H2O':[], 'CO2': [], 'O3': [], 'CH4':[], 'O2':[]}
+
+	for key in bins:
+		#truncate spectra
+		beg, end = bins[key]
+		earth_wave_trunc, earth_flux_trunc = truncate(beg, end, earth_wave, earth_flux)
+		mars_wave_trunc, mars_flux_trunc = truncate(beg, end, mars_wave, mars_flux)
+		jupiter_wave_trunc, jupiter_flux_trunc = truncate(beg, end, jupiter_wave, jupiter_flux)
+
+
+		#interpolate spectra (this returns an interpolation function that's a function of wavelength)
+		interp_bins_earth[key] = interp1d(earth_wave_trunc, earth_flux_trunc, kind='nearest', fill_value="extrapolate")
+		interp_bins_mars[key] = interp1d(mars_wave_trunc, mars_flux_trunc, kind='nearest', fill_value="extrapolate")
+		interp_bins_jupiter[key] = interp1d(jupiter_wave_trunc, jupiter_flux_trunc, kind='nearest', fill_value="extrapolate")
+
+
+
+	return interp_bins_earth, interp_bins_mars, interp_bins_jupiter
+
 def get_binned_Djs(earth_wave, earth_flux, mars_wave, mars_flux, jupiter_wave, jupiter_flux):
 
-    #clean spectra
-    earth_wave, earth_flux = clean(earth_wave, earth_flux)
-    mars_wave, mars_flux = clean(mars_wave, mars_flux)
-    jupiter_wave, jupiter_flux = clean(jupiter_wave, jupiter_flux)
 
 
-    #bins = {'H2O':[0.52, 0.72], 'CO2': [1.42, 1.58], 'O3': [0.94, 0.99], 'CH4':[0.73, 0.81], 'O2':[0.59, 0.70]}
-    bins = {'H2O':[0.5, 0.7], 'CO2': [1.4, 1.6], 'O3': [0.9, 1.0], 'CH4':[0.7, 0.8], 'O2':[0.6, 0.70]}
 
-    binned_Djs_mars = {'H2O': np.nan, 'CO2': np.nan, 'O3': np.nan, 'CH4':np.nan, 'O2':np.nan}
-    binned_Djs_jupiter = {'H2O': np.nan, 'CO2': np.nan, 'O3': np.nan, 'CH4':np.nan, 'O2':np.nan}
+	#bins = {'H2O':[0.52, 0.72], 'CO2': [1.42, 1.58], 'O3': [0.94, 0.99], 'CH4':[0.73, 0.81], 'O2':[0.59, 0.70]}
+	bins = {'H2O':[0.5, 0.7], 'CO2': [1.4, 1.6], 'O3': [0.9, 1.0], 'CH4':[0.7, 0.8], 'O2':[0.6, 0.70]}
 
-    for key in bins:
-        #truncate spectra
-        beg, end = bins[key]
-        earth_wave_trunc, earth_flux_trunc = truncate(beg, end, earth_wave, earth_flux)
-        mars_wave_trunc, mars_flux_trunc = truncate(beg, end, mars_wave, mars_flux)
-        jupiter_wave_trunc, jupiter_flux_trunc = truncate(beg, end, jupiter_wave, jupiter_flux)
+	binned_Djs_mars = {'H2O': np.nan, 'CO2': np.nan, 'O3': np.nan, 'CH4':np.nan, 'O2':np.nan}
+	binned_Djs_jupiter = {'H2O': np.nan, 'CO2': np.nan, 'O3': np.nan, 'CH4':np.nan, 'O2':np.nan}
 
 
-        #interpolate spectra (this returns an interpolation function that's a function of wavelength)
-        int_earth = interp1d(earth_wave_trunc, earth_flux_trunc, kind='nearest', fill_value="extrapolate")
-        int_mars = interp1d(mars_wave_trunc, mars_flux_trunc, kind='nearest', fill_value="extrapolate")
-        int_jupiter = interp1d(jupiter_wave_trunc, jupiter_flux_trunc, kind='nearest', fill_value="extrapolate")
-
-        #new, uniform array of wavelengths to use for all spectra
-        wave_new = np.linspace(beg, end, num=1000)
+	interp_bins_earth, interp_bins_mars, interp_bins_jupiter = get_binned_interp(earth_wave, earth_flux, mars_wave, mars_flux, jupiter_wave, jupiter_flux)
 
 
-        binned_Djs_mars[key] = djs(int_earth(wave_new),int_mars(wave_new))
-        binned_Djs_jupiter[key] = djs(int_earth(wave_new),int_jupiter(wave_new))
-    
-    return binned_Djs_mars, binned_Djs_jupiter
+	for key in bins:
+		##truncate spectra
+		beg, end = bins[key]
+		#earth_wave_trunc, earth_flux_trunc = truncate(beg, end, earth_wave, earth_flux)
+		#mars_wave_trunc, mars_flux_trunc = truncate(beg, end, mars_wave, mars_flux)
+		#jupiter_wave_trunc, jupiter_flux_trunc = truncate(beg, end, jupiter_wave, jupiter_flux)
+
+
+		##interpolate spectra (this returns an interpolation function that's a function of wavelength)
+		#int_earth = interp1d(earth_wave_trunc, earth_flux_trunc, kind='nearest', fill_value="extrapolate")
+		#int_mars = interp1d(mars_wave_trunc, mars_flux_trunc, kind='nearest', fill_value="extrapolate")
+		#int_jupiter = interp1d(jupiter_wave_trunc, jupiter_flux_trunc, kind='nearest', fill_value="extrapolate")
+
+
+		int_earth = interp_bins_earth[key]
+		int_mars = interp_bins_mars[key]
+		int_jupiter = interp_bins_jupiter[key]
+
+		#new, uniform array of wavelengths to use for all spectra
+		wave_new = np.linspace(beg, end, num=50)
+
+
+		binned_Djs_mars[key] = djs(int_earth(wave_new),int_mars(wave_new))
+		binned_Djs_jupiter[key] = djs(int_earth(wave_new),int_jupiter(wave_new))
+	
+
+	return binned_Djs_mars, binned_Djs_jupiter
 
 
 ##where to truncate wavelength so all spectra have same number of bins

main.py
@@ -15,8 +15,9 @@ if __name__ == '__main__':
 
 
     #What do you want to plot?
+    plt_binned_djs = True
     plt_spectra = False
-    plt_djs_dens = True
+    plt_djs_dens = False
 
 
 
@@ -30,11 +31,20 @@ if __name__ == '__main__':
     mars_wave, mars_flux = np.loadtxt('CatalogofSolarSystemObjects/Spectra/NativeResolution/Sun/Mars_McCord1971_Spec_Sun_HiRes.txt', unpack=True)
     jupiter_wave, jupiter_flux = np.loadtxt('CatalogofSolarSystemObjects/Spectra/NativeResolution/Sun/Jupiter_Lundock080507_Spec_Sun_HiRes.txt', unpack=True)
 
+
+    #clean spectra
+    earth_wave, earth_flux = clean(earth_wave, earth_flux)
+    mars_wave, mars_flux = clean(mars_wave, mars_flux)
+    jupiter_wave, jupiter_flux = clean(jupiter_wave, jupiter_flux)
+
     if normalize:
         avg_flux = np.mean(earth_flux)
         mars_flux = mars_flux * avg_flux/np.mean(mars_flux)
         jupiter_flux = jupiter_flux *avg_flux/np.mean(jupiter_flux)
 
+
+
+    bins = {'H2O':[0.5, 0.7], 'CO2': [1.4, 1.6], 'O3': [0.9, 1.0], 'CH4':[0.7, 0.8], 'O2':[0.6, 0.70]}
     binned_Djs_mars, binned_Djs_jupiter = get_binned_Djs(earth_wave, earth_flux, mars_wave, mars_flux, jupiter_wave, jupiter_flux)
 
     #most common isotopes of CO and H20 from HITRAN
@@ -73,6 +83,50 @@ if __name__ == '__main__':
     #-------------------------------------------------------------
 
 
+    #plot binned_djs
+    if plt_binned_djs:
+
+        interp_bins_earth, interp_bins_mars, interp_bins_jupiter = get_binned_interp(earth_wave, earth_flux, mars_wave, mars_flux, jupiter_wave, jupiter_flux)
+        iso_colors = {'H2O':'red', 'CO2': 'green', 'O3': 'blue', 'CH4':'purple', 'O2':'gray'}
+        fig, axs = plt.subplots(nrows=3, figsize=(5,10), sharex=True)
+
+
+        axs[0].scatter(earth_wave, earth_flux, label='Earth', s=5)
+        for key in interp_bins_earth:
+            beg, end = bins[key]
+            wave_new = np.linspace(beg, end, num=50)
+            axs[0].plot(wave_new, interp_bins_earth[key](wave_new), linestyle='dashed', color='black')
+            axs[0].axvspan(beg, end, alpha=0.5, label=key, color=iso_colors[key])
+
+
+
+        axs[1].scatter(mars_wave, mars_flux, label='Mars', s=5)
+        for key in interp_bins_mars:
+            beg, end = bins[key]
+            wave_new = np.linspace(beg, end, num=50)
+            axs[1].plot(wave_new, interp_bins_mars[key](wave_new), linestyle='dashed', color='black')
+            axs[1].axvspan(beg, end, alpha=0.5, label=key, color=iso_colors[key])
+
+
+        axs[2].scatter(jupiter_wave, jupiter_flux, label='Jupiter', s=5)
+        for key in interp_bins_jupiter:
+            beg, end = bins[key]
+            wave_new = np.linspace(beg, end, num=50)
+            axs[2].plot(wave_new, interp_bins_jupiter[key](wave_new), linestyle='dashed', color='black')
+            axs[2].axvspan(beg, end, alpha=0.5, label=key, color=iso_colors[key])
+
+        for ax in axs:
+            ax.set_ylim([-1e-11, 6e-11])
+            #ax.set_ylim([-34, -22])
+            ax.set_xscale('log')
+            ax.legend()
+            ax.set_ylabel('Flux (W/(m^2*Angstroms)')
+
+
+        #plt.xlabel('Wavelength (um)')
+        plt.ylabel('Flux (W/(m^2*Angstroms)')
+        plt.tight_layout()
+        plt.show()
 
 
     #------------- plot spectra --------------
@@ -105,10 +159,7 @@ if __name__ == '__main__':
         fig, axs = plt.subplots(nrows=3, figsize=(10,10), sharex=True)
 
 
-        #clean spectra
-        earth_wave, earth_flux = clean(earth_wave, earth_flux)
-        mars_wave, mars_flux = clean(mars_wave, mars_flux)
-        jupiter_wave, jupiter_flux = clean(jupiter_wave, jupiter_flux)
+
 
         #interpolate full spectra
         int_earth = interp1d(earth_wave, earth_flux, kind='nearest')